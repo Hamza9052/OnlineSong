313e4d70be93b0d46b94536016af33db0e50fa9f
Hamza <hamzh9052@gmail.com>
2024-12-16 00:16:28 +0100

    push Update

---
+++ app/src/main/java/online/song/onlinesong/Screens/PlaySong.kt
@@ -0,0 +1,139 @@
+package online.song.onlinesong.Screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import online.song.onlinesong.R
+import online.song.onlinesong.ViewModel.songVM
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun playSong(
+    viewModel: songVM,
+    navController: NavController,
+    name:String,
+    nameSong:String
+){
+    val songs = viewModel.ListSongs.observeAsState(emptyList<String>())
+    val isLoading = viewModel.SongsisLoading.observeAsState(Boolean)
+    val image = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(navController.context)
+            .data(viewModel.getImage(name,navController.context))
+            .crossfade(true)
+            .error(R.drawable.error)
+            .placeholder(R.drawable.error)
+            .build()
+    )
+
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = colorResource(R.color.background),
+                    titleContentColor = colorResource(R.color.White),
+                ),
+                title = {
+                    Text(
+                        nameSong,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = colorResource(R.color.White),
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate(Screen.Home.route) }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Localized description",
+                            tint = colorResource(R.color.icon),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                },
+                actions = {
+
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "Localized description",
+                            tint = colorResource(R.color.unfocus),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+
+                }
+
+            )
+        }
+    ) {padding ->
+        padding.calculateTopPadding()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxWidth()
+                .background(color = colorResource(R.color.background))
+
+        ) {
+            Image(
+                modifier = Modifier.size(250.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                painter = image,
+                contentDescription = stringResource(R.string.app_name),
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+
+            )
+
+
+
+
+        }
+    }
+
+}
