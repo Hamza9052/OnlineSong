0705ad77691f50cf42cf855ac71db83261a9bbb4
Hamza <hamzh9052@gmail.com>
2025-01-10 13:34:11 +0100

    push Update

--- app/src/main/java/online/song/onlinesong/App.kt
+++ app/src/main/java/online/song/onlinesong/App.kt
@@ -1,10 +1,18 @@
 package online.song.onlinesong
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.google.firebase.FirebaseApp
 import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 
+
+
+const val CHANNEL_ID = "channel_id"
+const val CHANNEL_NAME = "channel_name"
 
 class App:Application() {
     override fun onCreate() {
@@ -15,6 +23,12 @@ class App:Application() {
         firebaseAppCheck.installAppCheckProviderFactory(
             PlayIntegrityAppCheckProviderFactory.getInstance()
         )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
 
 
     }
