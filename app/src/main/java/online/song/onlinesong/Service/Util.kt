666f47a81bacf312ec148d6c5c56f7e461d9063e
Hamza <hamzh9052@gmail.com>
2025-01-11 00:13:44 +0100

    push Update

--- app/src/main/java/online/song/onlinesong/Service/Util.kt
+++ app/src/main/java/online/song/onlinesong/Service/Util.kt
@@ -2,6 +2,10 @@ package online.song.onlinesong.Service
 
 object Util {
 
+    const val ACTION_PLAY = "com.example.app.ACTION_PLAY"
+    const val ACTION_PAUSE = "com.example.app.ACTION_PAUSE"
+    const val ACTION_NEXT = "com.example.app.ACTION_NEXT"
+    const val ACTION_PREV = "com.example.app.ACTION_PREV"
     const val PLAYER = "player"
     const val SLIDER_CHANGE_VALUE = "slider_change_value"
     const val PLAYER_STATE_CHANNEL = "player_state_channel"
