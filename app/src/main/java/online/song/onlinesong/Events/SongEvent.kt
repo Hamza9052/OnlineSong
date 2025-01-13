2455ca191ec3054937e60545fee1b1fd0f49023a
Hamza <hamzh9052@gmail.com>
2025-01-13 15:28:05 +0100

    push Update

--- app/src/main/java/online/song/onlinesong/Events/SongEvent.kt
+++ app/src/main/java/online/song/onlinesong/Events/SongEvent.kt
@@ -1,15 +1,32 @@
 package online.song.onlinesong.Events
 
+import android.os.Parcelable
 import androidx.media3.exoplayer.ExoPlayer
+import kotlinx.android.parcel.Parcelize
 import online.song.onlinesong.LoginWithGoogle.SignResult
 import online.song.onlinesong.LoginWithGoogle.UserData
+import java.io.Serializable
 
-interface SongEvent {
-    data object PLAY: SongEvent
-    data object PAUSE: SongEvent
-    data object NEXT: SongEvent
-    data object PREV: SongEvent
-    data class Favorit(val name: String, val result: UserData,val singer:String): SongEvent
-    data class checkFavoriteSong(val result: UserData): SongEvent
-    data class ListSong(val list: List<String>): SongEvent
+@Parcelize
+sealed class SongEvent : Parcelable {
+    @Parcelize
+    object PLAY : SongEvent()
+
+    @Parcelize
+    object PAUSE : SongEvent()
+
+    @Parcelize
+    object NEXT : SongEvent()
+
+    @Parcelize
+    object PREV : SongEvent()
+
+    @Parcelize
+    data class Favorit(val name: String, val result: UserData, val singer: String) : SongEvent()
+
+    @Parcelize
+    data class CheckFavoriteSong(val result: UserData) : SongEvent()
+
+    @Parcelize
+    data class ListSong(val list: List<String>) : SongEvent()
 }
--- app/src/main/java/online/song/onlinesong/LoginWithGoogle/SignResult.kt
+++ app/src/main/java/online/song/onlinesong/LoginWithGoogle/SignResult.kt
@@ -1,12 +1,15 @@
 package online.song.onlinesong.LoginWithGoogle
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class SignResult(
     val data:UserData?,
     val errorMessage: String?
 )
-
+@Parcelize
 data class UserData(
     val userId: String?,
     val userName: String?,
     val ProfilePicUri: String?
-)
+) : Parcelable
