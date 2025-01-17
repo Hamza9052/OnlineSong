bda3d327a95db8775554c960650c2c7c73b58c3d
Hamza <hamzh9052@gmail.com>
2025-01-17 23:51:44 +0100

    push Update

--- app/src/main/java/online/song/onlinesong/MainActivity.kt
+++ app/src/main/java/online/song/onlinesong/MainActivity.kt
@@ -1,9 +1,6 @@
 package online.song.onlinesong
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Intent
-import android.os.Build
+
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -13,7 +10,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.Spring
@@ -33,7 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
-
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
@@ -43,6 +39,9 @@ import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 import kotlinx.coroutines.launch
 import online.song.onlinesong.BottomBar.bottomBar
 import online.song.onlinesong.LoginWithGoogle.GoogleAuthUiClient
@@ -52,14 +51,14 @@ import online.song.onlinesong.Screens.Screen
 import online.song.onlinesong.Screens.Search
 import online.song.onlinesong.Screens.listOfSongs
 import online.song.onlinesong.Screens.playSong
-import online.song.onlinesong.Service.MusicService
+import online.song.onlinesong.ViewModel.SongViewModelFactory
 import online.song.onlinesong.ViewModel.songVM
 import online.song.onlinesong.itemList.bottomIcons
 import online.song.onlinesong.ui.theme.OnlineSongTheme
-import java.lang.reflect.Type
 
 class MainActivity : ComponentActivity() {
-   private val VM by viewModels<songVM>()
+    private lateinit var VM: songVM
+    private var spotifyAppRemote: SpotifyAppRemote? = null
     private val googleAuthUiClient by lazy {
         GoogleAuthUiClient(
             context = applicationContext,
@@ -68,11 +67,8 @@ class MainActivity : ComponentActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            startForegroundService(Intent(this, MusicService::class.java))
-        }else{
-            startService(Intent(this, MusicService::class.java))
-        }
+
+        VM = ViewModelProvider(this, SongViewModelFactory(application)).get(songVM::class.java)
 
         enableEdgeToEdge()
         setContent {
@@ -295,16 +291,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        SpotifyAppRemote.connect(
+            this,
+            ConnectionParams.Builder("0102b15d08e642e884897fa1b5bb3223")
+                .setRedirectUri("http://localhost:8888/callback")
+                .showAuthView(true)
+                .build(),
+            object: Connector.ConnectionListener{
+                override fun onConnected(appRemote: SpotifyAppRemote?) {
+                    spotifyAppRemote = appRemote
+                    Log.d("Spotify", "Connected to Spotify")
+                }
 
-    }
+                override fun onFailure(throwable: Throwable?) {
+                    Log.e("Spotify", "Failed to connect to Spotify", throwable)
+                }
 
-    override fun onStart() {
-        super.onStart()
+            }
+        )
     }
 
     override fun onStop() {
         super.onStop()
+        SpotifyAppRemote.disconnect(spotifyAppRemote) // Disconnect to prevent leaks
     }
+
+
 }
 
 
