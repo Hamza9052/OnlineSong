311766969bf00b81006c9b23dea349db9b27c235
Hamza <hamzh9052@gmail.com>
2025-01-19 02:21:58 +0100

    push Update

--- app/src/main/java/online/song/onlinesong/Api.kt
+++ app/src/main/java/online/song/onlinesong/Api.kt
@@ -1,22 +1,27 @@
 package online.song.onlinesong
 
 import okhttp3.Request
-import com.cloudinary.android.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import org.json.JSONObject
 import android.util.Base64
-
+import android.util.Log
+import online.song.onlinesong.BuildConfig
 class Api {
     private val apiUrl = "https://accounts.spotify.com/api/token"
 
     // Function to fetch access token from Spotify using client credentials
     suspend fun getAccessToken(): Result<String> {
-        val clientId = R.string.SPOTIFY_CLIENT_ID
-        val clientSecret = R.string.SPOTIFY_CLIENT_SECRET
-        
+        // Replace with actual client ID and client secret
+        val clientId = BuildConfig.SPOTIFY_CLIENT_ID
+        val clientSecret = BuildConfig.SPOTIFY_CLIENT_SECRET
+
+        // Check if clientId or clientSecret are empty
+        if (clientId.isBlank() || clientSecret.isBlank()) {
+            return Result.failure(Exception("Client ID or Client Secret is not provided"))
+        }
 
         // Prepare authorization header
         val authHeader = "Basic " + Base64.encodeToString(
@@ -51,14 +56,17 @@ class Api {
                     if (accessToken != null) {
                         Result.success(accessToken)
                     } else {
+                        Log.e("Token_Error", "Access token not found in response: $jsonResponse")
                         Result.failure(Exception("Access token not found"))
                     }
                 } else {
+                    Log.e("Token_Error", "API call failed: ${response.code} - ${response.message}")
                     Result.failure(Exception("Failed to fetch access token: ${response.message}"))
                 }
             }
             response
         } catch (e: Exception) {
+            Log.e("Token_Error", "Exception occurred: ${e.message}")
             Result.failure(e) // Return failure result on exception
         }
     }
